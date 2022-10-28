c042b7303b48615afb7106424d70fa2f70bc9907
Jordano Utrera <axeljordanomu@gmail.com>
2022-10-28 11:13:07 -0500

    cambio de ventana en login

--- PassThePenClient/PassThePen/Login.xaml.cs
+++ PassThePenClient/PassThePen/Login.xaml.cs
@@ -60,10 +60,8 @@ namespace PassThePen
 
         private void Button_Forgot_Password_Click(object sender, RoutedEventArgs e)
         {
-            /*RecoverPassword recover = new RecoverPassword();
-            recover.Show();*/
-            MainMenu menu = new MainMenu();
-            menu.Show();
+            RecoverPassword recover = new RecoverPassword();
+            recover.Show();
         }
 
         private void Button_Exit_Click(object sender, RoutedEventArgs e)
